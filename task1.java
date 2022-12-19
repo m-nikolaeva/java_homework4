42149b4dd63f89625cb0a2a22eba5639e90d7108
Marina <mv.nikolaeva1507@gmail.com>
2022-12-19 16:54:56 +0300

    add java_homework4

---
+++ task1.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+/* Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список. */
+
+public class task1 {
+    public static void main(String[] args) {
+        LinkedList<String> taskList = new LinkedList<>();
+        taskList.add("Один");
+        taskList.add("2");
+        taskList.add("3");
+        taskList.add("Пять");
+        taskList.add("7");
+        System.out.println("Изначальный список:");
+        System.out.println(taskList.toString());
+        System.out.println("Перевернутый список:");
+        System.out.println(reverseLst(taskList).toString());
+
+    }
+
+    public static LinkedList<String> reverseLst(LinkedList<String> lst) {
+        LinkedList<String> resultLst = new LinkedList<>();
+        for (int i = lst.size() - 1; i >= 0; i--) {
+            resultLst.add(lst.get(i));
+        }
+        return resultLst;
+    }
+
+}
---
+++ task2.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+
+/* Реализуйте очередь с помощью LinkedList со следующими методами: 
+enqueue() - помещает элемент в конец очереди, 
+dequeue() - возвращает первый элемент из очереди и удаляет его, 
+first() - возвращает первый элемент из очереди, не удаляя. */
+
+import java.util.Deque;
+
+public class task2 {
+    public static void main(String[] args) {
+        Deque<Object> line = new LinkedList<>();
+        task2 myLine2 = new task2();
+        line.push("T");
+        line.push("a");
+        line.push("e");
+        line.push("r");
+        line.push("G");
+        line.push("!");
+        line.push("?");
+        line.push("!");
+        System.out.printf("Текущая очередь: %s\n", line);
+        myLine2.enqueue(line);
+        System.out.printf("Очередь после перемещения первого элемента: %s \n", line);
+        myLine2.dequeue(line);
+        System.out.printf("Очередь после удаления первого элемента: %s \n", line);
+        myLine2.first(line);
+        System.out.printf("Очередь после возвращения 1 элеменита без его удаления: %s \n", line);
+    }
+    public void first(Deque<Object> object){
+        System.out.println("Первый элемент очереди: " + object.getFirst());
+    }
+
+    private void enqueue(Deque<Object> object){
+        object.addLast(object.pollFirst());
+
+    }
+    private void dequeue(Deque<Object> object){
+        System.out.println("Удаляем первый элемент очереди: " + object.removeFirst());
+    }
+    
+}
